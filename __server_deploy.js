6d9b8d336a02397b89875c207b41099d6a2e0a62
Sajeda Akhter <sajedasaju85@gmail.com>
2022-04-27 21:55:58 +0600

    adding steps

---
+++ __server_deploy.js
@@ -0,0 +1,13 @@
+/***
+ * -----------------------------------
+ * one time for your computer
+ * -----------------------------------
+ * 1.create heroku account
+ * 2.verify email
+ * 3.install heroku cli
+ * 4.heroku login
+ * -----------------------------------
+ * for each project one time
+ * ------------------------------------
+ * 1.heroku create
+ */
